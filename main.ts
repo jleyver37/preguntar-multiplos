e51e91f709e77a1a7c75becd5dc63b0edca8bd17
jleyver37 <90491236+jleyver37@users.noreply.github.com>
2024-08-30 14:57:28 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let Numero = 0
 let Contador = 0
 input.onButtonPressed(Button.A, function () {
-    Numero = 0
-    Numero += randint(1, 6)
-    Contador = 1
-    for (let index = 0; index < Numero; index++) {
-        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+    Numero += randint(2, 8)
+    basic.showNumber(Numero)
+    basic.clearScreen()
+    for (let index = 0; index < 10; index++) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -46,31 +46,36 @@ input.onButtonPressed(Button.A, function () {
             # # # # #
             `)
         basic.clearScreen()
+        basic.pause(1000)
+        Contador += 1
+        basic.showNumber(Numero * Contador)
         basic.pause(1000)
         basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    Numero = 0
-    Numero += randint(1, 6)
-    Contador = 1
-    for (let index = 0; index < Numero; index++) {
+    Numero += randint(2, 8)
+    basic.showNumber(Numero)
+    basic.clearScreen()
+    for (let index = 0; index < 10; index++) {
         music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
-        basic.showNumber(Contador)
+        basic.pause(1000)
         Contador += 1
+        basic.showNumber(Numero * Contador)
+        basic.pause(1000)
         basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.B, function () {
-    Numero = 0
-    Numero += randint(1, 6)
-    Contador = 1
-    for (let index = 0; index < Numero; index++) {
+    Numero += randint(2, 8)
+    basic.showNumber(Numero)
+    basic.clearScreen()
+    for (let index = 0; index < 10; index++) {
         music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
         basic.showLeds(`
             . . . . .
             . . . . .
-            . . # . .
+            . . # . #
             . . . . .
             . . . . .
             `)
@@ -108,9 +113,5 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.clearScreen()
         basic.pause(1000)
-        basic.showNumber(Contador)
-        Contador += 1
-        basic.pause(1000)
-        basic.clearScreen()
     }
 })
